c8aa55fc3c44e86efccfac9ec43c7b3503711308
Sam Salisbury <samsalisbury@gmail.com>
2014-07-25 15:26:00 +0100

    Minor tidy up

--- errors.go
+++ errors.go
@@ -12,19 +12,23 @@ func (err HalgoError) Error() string {
 	return err.Message
 }
 
+func Error(args ...interface{}) HalgoError {
+	message := fmt.Sprint(args...)
+	//Print("ERROR RAISED:", message)
+	return HalgoError{message}
+}
+
 type HTTPError struct {
 	StatusCode int
 	HalgoError
 }
 
-func Error404(what string) HTTPError {
-	return HTTPError{404, Error(what + " not found.")}
+func HttpError(statusCode int, args ...interface{}) HTTPError {
+	return HTTPError{statusCode, Error(args...)}
 }
 
-func Error(args ...interface{}) HalgoError {
-	message := fmt.Sprint(args...)
-	//Print("ERROR RAISED:", message)
-	return HalgoError{message}
+func Error404(what string) HTTPError {
+	return HttpError(404, what+" not found.")
 }
 
 func Errorf(format string, args ...interface{}) error {

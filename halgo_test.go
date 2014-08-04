815d4b4cde20af094f754435cab283a3c87df8a7
Sam Salisbury <samsalisbury@gmail.com>
2014-08-04 21:23:22 +0100

    Add httpspec

--- halgo_test.go
+++ halgo_test.go
@@ -1,15 +1,23 @@
 package halgo
 
 import (
+	httpspec "github.com/opentable/http-specs"
 	"net/http"
+	"testing"
 )
 
-func Example() {
+func Test_Example(t *testing.T) {
 	if server, err := NewServer(RootResource{}); err != nil {
 		Print(err)
 	} else {
 		Print("Listening on :8080")
-		http.ListenAndServe(":8080", server)
+		go http.ListenAndServe(":8080", server)
+		if spec, err := httpspec.NewSpec("halgo.httpspec"); err != nil {
+			t.Error(err)
+		} else {
+			spec.Run(t, "http://localhost:8080")
+		}
+
 	}
 	// Output:
 	// not this!
